a19cd2d41f9dfcbf0c5af51300dc0657bed8c970
YASIR AHMAD <yazirahmed123@gmail.com>
2025-07-04 16:55:28 +0300

    Day_01: Project Initialized

---
+++ database/migrations/2025_07_04_133516_create_hospitals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('address');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->string('region')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hospitals');
+    }
+};
